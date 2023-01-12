a4a9f1770f19303ae06dfd4d80b9c47b2f5b58ec
phucgari <phucgari@gmail.com>
2023-01-12 22:16:55 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Point3D d3=new Point3D(1,3,4);
+        System.out.println(d3);
+        Point2D d2=new Point3D(2,1,4);
+        System.out.println(d2);
+    }
+}
---
+++ src/Point3D.java
@@ -0,0 +1,32 @@
+public class Point3D extends Point2D{
+    private float z;
+    Point3D(float x,float y,float z){
+        super(x,y);
+        this.z=z;
+    }
+    Point3D(){
+        super();
+        z=0;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+    public void setXYZ(float x,float y,float z){
+        super.setXY(x,y);
+        this.z=z;
+    }
+    public float[] getXYZ(){
+        float[] arr={getX(),getY(),getZ()};
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+","+z;
+    }
+}
